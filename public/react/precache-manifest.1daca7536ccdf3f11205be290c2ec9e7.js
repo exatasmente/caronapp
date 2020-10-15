99f23836d6f8f26d3f1dbf581974324028379b28
Luiz Neto <luizneto@rits.com.br>
2020-10-15 19:21:03 -0300

    assets

---
+++ public/react/precache-manifest.1daca7536ccdf3f11205be290c2ec9e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ed70bf4e6168f65e65",
+    "url": "./react/static/css/main.3c3d4681.chunk.css"
+  },
+  {
+    "revision": "b5ed70bf4e6168f65e65",
+    "url": "./react/static/js/main.b5ed70bf.chunk.js"
+  },
+  {
+    "revision": "a9f05c18b8f56cf96fb2",
+    "url": "./react/static/css/1.9bde3c54.chunk.css"
+  },
+  {
+    "revision": "a9f05c18b8f56cf96fb2",
+    "url": "./react/static/js/1.a9f05c18.chunk.js"
+  },
+  {
+    "revision": "decc047a33a39582234d",
+    "url": "./react/static/js/runtime~main.decc047a.js"
+  },
+  {
+    "revision": "3762619f18d4212ce3a73254dac36236",
+    "url": "./react/static/media/logo.3762619f.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "82055e2ccefea459a59d0e40ffe12eed",
+    "url": "./react/index.html"
+  }
+];
